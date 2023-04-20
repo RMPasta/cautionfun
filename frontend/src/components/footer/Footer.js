723ae261583bf6e340025e8292c3618dcab650de
rmpasta <rmpasta.code@gmail.com>
2023-04-19 21:21:10 -0400

    Revert "add copy to clipboard for addresses"
    This reverts commit 295afa945efbf391d2427955f94c3b5da1a4eb8d.

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -2,25 +2,6 @@ import React from "react";
 import "./footer.css";
 
 export default function Footer() {
-
-  function copyElementText(id1, id2) {
-    let text = document.getElementById(id1).innerText;
-    let elem = document.createElement("textarea");
-    let clipboard = document.querySelector(`.${id2}`);
-    clipboard.classList.remove("fa-clipboard");
-    clipboard.classList.add("fa-clipboard-check")
-    setTimeout(() => {
-    clipboard.classList.remove("fa-clipboard-check");
-    clipboard.classList.add("fa-clipboard")
-    }, 2000)
-
-    document.body.appendChild(elem);
-    elem.value = text;
-    elem.select();
-    document.execCommand("copy");
-    document.body.removeChild(elem);
-}
-
   return (
     <div className="footer">
       <ul className="socials">
@@ -62,6 +43,7 @@ export default function Footer() {
             src="https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg"
             alt="Bitcoin logo"
           />
+          <p>BTC: bc1qutvrfse6q4n3073lcr3thpx46xyr9tx50nz3h9</p>
           <div className="addy">BTC: <p id="btc-addy">bc1qutvrfse6q4n3073lcr3thpx46xyr9tx50nz3h9</p><i className="fa-solid fa-clipboard btc" onClick={() => copyElementText("btc-addy", "btc")}></i></div>
         </div>
         <div className="address">
@@ -69,6 +51,7 @@ export default function Footer() {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/1200px-Ethereum_logo_2014.svg.png"
             alt="Ethereum logo"
           />
+          <p>ETH: 0x98c0fe160b049d3436d2c3c26bf454fcfad2a8c1</p>
           <div className="addy">ETH: <p id="eth-addy">0x98c0fe160b049d3436d2c3c26bf454fcfad2a8c1</p><i className="fa-solid fa-clipboard eth" onClick={() => copyElementText("eth-addy", "eth")}></i></div>
         </div>
         <div className="address">
@@ -76,6 +59,7 @@ export default function Footer() {
             src="https://imgs.search.brave.com/CNStDCGflYL-LvUKXNdzBftkV4eFGjQ0h-PjOL4nJEg/rs:fit:250:249:1/g:ce/aHR0cHM6Ly9jb2lu/YXJiaXRyYWdlYm90/LmNvbS9pbmMvY29p/bl9sb2dvcy92ZXJ1/cy1jb2luLnBuZw"
             alt="Verus Coin logo"
           />
+          <p>VRSC: RGabSv4JLkvUeQQbCFED18gDDLGU8STrbs</p>
           <div className="addy">VRSC: <p id="vrsc-addy">RGabSv4JLkvUeQQbCFED18gDDLGU8STrbs</p><i className="fa-solid fa-clipboard vrsc" onClick={() => copyElementText("vrsc-addy", "vrsc")}></i></div>
         </div>
       </div>
