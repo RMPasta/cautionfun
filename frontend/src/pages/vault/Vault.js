00d32c02862f960c3d534f12da6836e4df47c14e
RMPasta <rmpasta.code@gmail.com>
2023-04-21 21:05:11 -0400

    add vault filters

--- frontend/src/pages/vault/Vault.js
+++ frontend/src/pages/vault/Vault.js
@@ -3,8 +3,14 @@ import GUCard from "../../components/gu-card/GUCard";
 import "./vault.css";
 
 export default function Vault() {
-  const [cards, setCards] = useState({});
+  const [cards, setCards] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [filtered, setFiltered] = useState([]);
+  const [selectedGod, setSelectedGod] = useState("All");
+  const [selectedQuality, setSelectedQuality] = useState("All");
+  const [selectedRarity, setSelectedRarity] = useState("All");
+  const [selectedSet, setSelectedSet] = useState("All");
+
   useEffect(() => {
     const getCards = async () => {
       const response = await fetch(
@@ -26,7 +32,7 @@ export default function Vault() {
             cardsObj[img] = card;
             cardsObj[img]["count"] = 1;
           }
-          setCards(cardsObj);
+          setCards(Object.values(cardsObj));
         });
       } else {
         console.log("crap! fetch went bad");
@@ -36,26 +42,150 @@ export default function Vault() {
     getCards();
   }, []);
 
-  const sortedByDeath = Object.values(cards).sort((a, b) => {
-    return a.metadata.god - b.metadata.god
-  })
-  console.log(cards)
+  useEffect(() => {
+    let filteredCards = cards;
+
+    if (selectedGod !== "All") {
+      filteredCards = filteredCards.filter(
+        (card) => card.metadata.god === selectedGod
+      );
+    }
+
+    if (selectedQuality !== "All") {
+      filteredCards = filteredCards.filter(
+        (card) => card.metadata.quality === selectedQuality
+      );
+    }
+
+    if (selectedRarity !== "All") {
+      filteredCards = filteredCards.filter(
+        (card) => card.metadata.rarity === selectedRarity
+      );
+    }
+
+    if (selectedSet !== "All") {
+      filteredCards = filteredCards.filter(
+        (card) => card.metadata.set === selectedSet
+      );
+    }
+
+    setFiltered(filteredCards);
+  }, [selectedGod, selectedQuality, selectedRarity, selectedSet, cards]);
+
+  function handleGodFilterClick(god) {
+    setSelectedGod(god);
+  }
+
+  function handleRarityFilterClick(rarity) {
+    if (rarity !== "All") {
+      setSelectedRarity(rarity);
+    } else {
+      setSelectedRarity("All");
+    }
+  }
+
+  function handleQualityFilterClick(quality) {
+    if (quality !== "All") {
+      setSelectedQuality(quality);
+    } else {
+      setSelectedQuality("All");
+    }
+  }
+
+  function handleSetFilterClick(set) {
+    if (set !== "All") {
+      setSelectedSet(set);
+    } else {
+      setSelectedSet("All");
+    }
+  }
+
+  console.log(filtered);
   return (
-    <div className="vault">
-      {!isLoaded ? (
-        <h1>...Loading</h1>
-      ) : (
-        cards &&
-        sortedByDeath.map((card) => (
-          <li key={card.image_url}>
-            <GUCard
-              imgUrl={card.image_url}
-              name={card.name}
-              quantity={card.count}
-            />
-          </li>
-        ))
-      )}
+    <div>
+      <div className="filters">
+        <div className="filter-box">
+          {["All", "neutral", "nature", "death", "war", "magic"].map((god) => (
+            <button
+              key={god}
+              onClick={() => handleGodFilterClick(god)}
+              className={
+                selectedGod === god ? "button current-filter" : "button"
+              }
+            >
+              {god[0].toUpperCase() + god.slice(1)}
+            </button>
+          ))}
+        </div>
+        <div className="filter-box">
+          {["All", "Meteorite", "Shadow", "Gold", "Diamond"].map((quality) => (
+            <button
+              key={quality}
+              onClick={() => handleQualityFilterClick(quality)}
+              className={
+                selectedQuality === quality ? "button current-filter" : "button"
+              }
+            >
+              {quality[0].toUpperCase() + quality.slice(1)}
+            </button>
+          ))}
+        </div>
+        <div className="filter-box">
+          {["All", "common", "rare", "epic", "legendary", "mythic"].map(
+            (rarity) => (
+              <button
+                key={rarity}
+                onClick={() => handleRarityFilterClick(rarity)}
+                className={
+                  selectedRarity === rarity ? "button current-filter" : "button"
+                }
+              >
+                {rarity[0].toUpperCase() + rarity.slice(1)}
+              </button>
+            )
+          )}
+        </div>
+        <div className="filter-box">
+          {[
+            "All",
+            "genesis",
+            "etherbots",
+            "trial",
+            "core",
+            "verdict",
+            "wander",
+            "wolf",
+          ].map((set) => (
+            <button
+              key={set}
+              onClick={() => handleSetFilterClick(set)}
+              className={
+                selectedSet === set ? "button current-filter" : "button"
+              }
+            >
+              {set[0].toUpperCase() + set.slice(1)}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="vault">
+        {!isLoaded ? (
+          <h1>...Loading</h1>
+        ) : filtered.length > 0 ? (
+          cards &&
+          filtered.map((card) => (
+            <li key={card.image_url}>
+              <GUCard
+                imgUrl={card.image_url}
+                name={card.name}
+                quantity={card.count}
+              />
+            </li>
+          ))
+        ) : (
+          <div className="nothing-filtered">Nothing to see here!</div>
+        )}
+      </div>
     </div>
   );
 }
