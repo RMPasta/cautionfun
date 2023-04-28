17bd736a5cc22308a563cbd0fdcb66144543debe
rmpasta <rmpasta.code@gmail.com>
2023-04-28 18:09:07 -0400

    trying to figure out imx stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Ref from "./pages/ref-links/Ref";
 import Home from "./pages/home/Home";
 import Vault from "./pages/vault/Vault";
 import VerusTest from "./pages/verustest/VerusTest";
+import IMX from "./components/imx/IMX";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -16,6 +17,7 @@ function App() {
         <Route path="/ref-links" element={<Ref />} />
         <Route path="/vault" element={<Vault />} />
         <Route path="/verus" element={<VerusTest />} />
+        <Route path="/imx" element={<IMX />} />
       </Routes>
       <Footer />
     </>
--- frontend/src/components/imx/IMX.js
+++ frontend/src/components/imx/IMX.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react'
-import { ImmutableXClient, Link } from '@imtbl/imx-sdk';
+import React, { useEffect, useState } from 'react'
+import { ImmutableXClient } from '@imtbl/imx-sdk';
 // import libExample from './imx-client-test';
 
 export default function IMX() {
+
+  const [userAssets, setUserAssets] = useState([])
+
   useEffect(() => {
     async function libExample() {
       const client = await ImmutableXClient.build({
@@ -17,15 +20,30 @@ export default function IMX() {
         const assets = await client.getAssets({
           user: address,
         })
-        console.log("balances: ", balances, " orders: ", orders, " assets: ", assets)
+        // console.log("balances: ", balances, " orders: ", orders, " assets: ", assets)
       }
+
     }
     libExample()
   }, [])
+useEffect(() => {
+  async function getUserAssets(assetCursor) {
+    const client = await ImmutableXClient.build({
+      publicApiUrl: 'https://link.x.immutable.com',
+    })
+    const address = "0x5989Cc6bbAc8f8E3afc4143bFE57ba68Ef20F84F"
+    const assetsRequest = await client.getAssets({ user: address, cursor: assetCursor, status: 'imx', collection: "0xacb3c6a43d15b907e8433077b6d38ae40936fe2c" });
+    setUserAssets(assetsRequest.result)
+    console.log(assetsRequest)
+    return { assets: assetsRequest.result, cursor: assetsRequest.cursor };
+ }
+ getUserAssets()
 
+}, [])
   return (
     <div>
         <h1>IMX Testing</h1>
+        <div>{userAssets}</div>
     </div>
   )
 }
