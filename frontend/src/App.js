8e5e251c0618331675c8531f527675c42df6228b
rmpasta <rmpasta.code@gmail.com>
2023-04-30 14:30:06 -0400

    style layout for caution coin page, add button to nav bar, rename component etc, have icon link to home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Ref from "./pages/ref-links/Ref";
 import Home from "./pages/home/Home";
 import Vault from "./pages/vault/Vault";
-import VerusTest from "./pages/verustest/VerusTest";
+import CC from "./pages/cc/CC";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/ref-links" element={<Ref />} />
         <Route path="/vault" element={<Vault />} />
-        <Route path="/verus" element={<VerusTest />} />
+        <Route path="/cc" element={<CC />} />
       </Routes>
       <Footer />
     </>
--- frontend/src/pages/cc/CC.js
+++ frontend/src/pages/cc/CC.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import './CC.css'
 
-export default function VerusTest() {
+export default function CC() {
 // const [address, setAddress] = useState('');
 const [price, setPrice] = useState('');
-const [info, setInfo] = useState('');
+const [supply, setSupply] = useState('');
 const [claims, setClaims] = useState('');
+const [reserves, setReserves] = useState('');
+const [ccIds, setCcIds] = useState('');
+const [holders, setHolders] = useState('');
 
   // useEffect(() => {
   //   axios.get('http://127.0.0.1:5000/')
@@ -34,7 +38,7 @@ const [claims, setClaims] = useState('');
     axios.get('http://127.0.0.1:5000/ccinfo')
       .then((response) => {
         // console.log(response)
-        setInfo(response.data);
+        setSupply(response.data);
       })
       .catch((error) => {
         console.error(error);
@@ -52,10 +56,24 @@ const [claims, setClaims] = useState('');
       });
   }, []);
   return (
-    <div>
-      <h1>Price: {price ? price : "...Loading"}</h1>
-      <h1>Info: {info ? info : "...Loading"}</h1>
-      <h1>Claims: {claims ? claims : "...Loading"}</h1>
+    <div className='cautioncoin'>
+      <h1 className='cc-header'>CautionCoin Status</h1>
+      <div className='economics'>
+        <h2 className='economics-header'>Economics:</h2>
+        <p>Current Supply: {supply ? supply : "...Loading"}</p>
+        <p>Current Reserves: {reserves ? reserves : "...Loading"}</p>
+        <p>Current Price: {price ? price : "...Loading"} VRSC</p>
+      </div>
+      <div className='general-info'>
+        <h2 className='info-header'>General Info:</h2>
+        <p>Airdrop Claims Remaining: {claims ? claims : "...Loading"}</p>
+        <p>CC.IDs Registered: {ccIds ? ccIds : "...Loading"}</p>
+        {/* <p>Top CC Holders: {holders ? holders : "...Loading"}</p> */}
+        <div>
+        <p>Top CC Holders: </p>
+        {[1, 2, 3, 4, 5].map(holder => <p>Tokens: {holder} --- Discord Tag: User{holder}</p>).reverse()}
+        </div>
+      </div>
     </div>
   );
 }
