276d11548797372d04d5e2c88d1622680e832afc
rmpasta <rmpasta.code@gmail.com>
2023-05-08 21:35:54 -0400

    add caution info component

---
+++ frontend/src/pages/cc/CC.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+// import axios from 'axios'w
+import './CC.css'
+
+export default function CC() {
+// const [address, setAddress] = useState(localStorage.getItem("generated-wallet") || '');
+
+  return (
+    <div className='cautioncoin'>
+      <h2>CautionCoin: A Peer-to-Peer Electronic Social Token System</h2>
+      <p>CautionCoin is a social token designed to reward our most supportive community members, incentivize participation, and return value to our ecosystem.</p>
+      <p>CautionCoin is built on the Verus blockchain, a secure, scalable, and permissionless platform that provides a foundation for the creation of social tokens with true underlying value.</p>
+      <p>Unlike most social tokens, CautionCoin will launch with inherent value that does not depend upon the efforts or actions of the creator. And as the ecosystem scales, clear paths to increase</p>
+      <p>the system's inherent value are defined, and value is not limited by time and energy. The potential future utility of CautionCoin is limited only by our collective imaginations.</p>
+      <p>I’d like to invite you to join the community and help shape the future of CautionCoin.</p>
+      <p>Launching June 9th.</p>
+      ___
+      <h2>How to prepare:</h2>
+      <p>Download a Verus wallet from https://verus.io/wallet</p>
+      <p>-Guide: https://peakd.com/verus/@cautionfun/getting-set-up-with-verus-desktop-wallet</p>
+      <p>-Back up your wallet: https://wiki.verus.io/#!how-to/how-to_backup_my_wallet.md</p>
+      <p>Join the TeamStreamTeam discord: https://discord.gg/2cKPdAkUCq</p>
+      <p>Follow me on Twitch: https://twitch.tv/cautionfun_gu</p>
+      <p>And Twitter: https://twitter.com/cautionfun_gu</p>
+    </div>
+  );
+}
