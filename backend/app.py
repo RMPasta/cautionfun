773a399a07a8cfc5b41a4eedb7f1386ce9554298
RMPasta <rmpasta.code@gmail.com>
2023-07-11 13:48:39 -0400

    add db for users, routes to get users and update a user, and begin discord log in implementation

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,173 @@
-from flask import Flask, render_template
-from cc_price import price
-from cc_info import ccinfo
-from cc_claims import claimsleft
-from testrpc import testrpc
-from flask_cors import CORS
+import os
+import csv
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+# from cc_price import price
+# from cc_info import ccinfo
+# from cc_claims import claimsleft
+# from testrpc import testrpc
+# from flask_cors import CORS
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:3000'])
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# CORS(app, origins=['http://localhost:3000'])
 
-@app.route('/getaddress')
-def getaddress():
-    data = testrpc()
-    return str(data)
+db = SQLAlchemy(app)
 
-@app.route('/price')
-def getprice():
-    data = price()
-    return str(data)
+DISCORD_CLIENT_ID = '1094275757397786804'
+DISCORD_CLIENT_SECRET = 'KFsknpPWv0Pww8LDh1js8viS1nGnGqYN'
 
-@app.route('/ccinfo')
-def getccinfo():
-    data = ccinfo()
-    return str(data)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    twitch_id = db.Column(db.Integer)
+    twitch_username = db.Column(db.String)
+    discord_id = db.Column(db.Integer)
+    discord_username = db.Column(db.String)
+    verus = db.Column(db.String)
+    eth = db.Column(db.Integer)
+    rental = db.Column(db.Integer)
+    points = db.Column(db.Integer)
+    airdrop_amount = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=func.now())
+    bio = db.Column(db.Text)
 
-@app.route('/claimsleft')
-def getclaimleft():
-    data = claimsleft()
-    return str(data)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'twitch_id': self.twitch_id,
+            'twitch_username': self.twitch_username,
+            'discord_id': self.discord_id,
+            'discord_username': self.discord_username,
+            'verus': self.verus,
+            'eth': self.eth,
+            'rental': self.rental,
+            'points': self.points,
+            'airdrop_amount': self.airdrop_amount,
+            'created_at': self.created_at,
+        }
+
+    def __repr__(self):
+        return f'<User {self.id}>'
+
+# function to loop through csv user file and enter those users into the database
+# with app.app_context():
+#     def add_users_from_csv():
+#         with open('user_info.csv', 'r') as file:
+#             reader = csv.DictReader(file)
+#             for row in reader:
+#                 user = User(
+#                     twitch_id=row['Twitch ID Number'],
+#                     twitch_username=row['Twitch Username'],
+#                     discord_id=row['DiscordID#'],
+#                     discord_username=row['Discord Username'],
+#                     verus=row['VRSC Address'],
+#                     eth=row['ETH address'],
+#                     rental=row['Rental Wallet'],
+#                     points=row['Points'],
+#                     airdrop_amount=row['Airdrop Amount'],
+#                 )
+#                 db.session.add(user)
+#             db.session.commit()
+#         return 'Users added to the database'
+
+#     print(add_users_from_csv())
+
+@app.route('/auth/discord/callback')
+def discord_callback():
+    code = request.args.get('code')
+
+    data = {
+        'client_id': DISCORD_CLIENT_ID,
+        'client_secret': DISCORD_CLIENT_SECRET,
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+        'scope': 'identify'
+    }
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    # Exchange the authorization code for an access token
+    response = requests.post('https://discord.com/api/oauth2/token', data=data, headers=headers)
+    access_token = response.json().get('access_token')
+
+    # Use the access token to fetch user information
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    user_response = requests.get('https://discord.com/api/users/@me', headers=headers)
+    user_data = user_response.json()
+
+    # Do something with the user data (e.g., store it in the database)
+
+    return redirect('/')  # Redirect back to the homepage
+
+
+@app.route('/')
+def landing():
+    return "hello?"
+    # users = User.query.all()
+    # return {'users': [user.to_dict() for user in users]}
+
+@app.route('/user/update/<int:id>', methods=['PUT'])
+def update_user(id):
+    data = request.get_json(force=True)
+    user = User.query.get(id)
+
+    if 'twitch_id' in data:
+        user.twitch_id = data['twitch_id']
+    if 'twitch_username' in data:
+        user.twitch_username = data['twitch_username']
+    if 'discord_id' in data:
+        user.discord_id = data['discord_id']
+    if 'discord_username' in data:
+        user.discord_username = data['discord_username']
+    if 'verus' in data:
+        user.verus = data['verus']
+    if 'eth' in data:
+        user.eth = data['eth']
+    if 'rental' in data:
+        user.rental = data['rental']
+    if 'points' in data:
+        user.points = data['points']
+    if 'airdrop_amount' in data:
+        user.airdrop_amount = data['airdrop_amount']
+    if 'bio' in data:
+        user.bio = data['bio']
+
+    db.session.commit()
+
+    return user.to_dict()
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get(id)
+    return user.to_dict()
+
+# @app.route('/getaddress')
+# def getaddress():
+#     data = testrpc()
+#     return str(data)
+
+# @app.route('/price')
+# def getprice():
+#     data = price()
+#     return str(data)
+
+# @app.route('/ccinfo')
+# def getccinfo():
+#     data = ccinfo()
+#     return str(data)
+
+# @app.route('/claimsleft')
+# def getclaimleft():
+#     data = claimsleft()
+#     return str(data)
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
-import SupaBaseAuth from "../../components/supabase-auth/SupaBaseAuth";
-import { createClient } from "@supabase/supabase-js";
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
   const [user, setUser] = useState("Sign In");
+  const discordAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=1094275757397786804&redirect_uri=localhost%3A3000&response_type=code&scope=identify`;
   const openMenu = () => {
     if (showMenu) return;
     setShowMenu(true);
@@ -17,41 +16,50 @@ export default function Header() {
     setShowMenu(false);
   };
 
-  const supabase = createClient(
-    process.env.REACT_APP_SUPABASE_URL,
-    process.env.REACT_APP_ANON_KEY
-  );
+  // const supabase = createClient(
+  //   process.env.REACT_APP_SUPABASE_URL,
+  //   process.env.REACT_APP_ANON_KEY
+  // );
 
-  useEffect(() => {
-    const getUserData = async () => {
-      await supabase.auth.getUser().then((value) => {
-        if (value.data?.user) {
-          if (value.data.user.identities[0].identity_data.name) {
-            //set discord name
-            setUser(value.data.user.identities[0].identity_data.name);
-          } else {
-            //set email name
-            setUser(value.data.user.identities[0].identity_data.email.split("@")[0]);
-        }
-        }
-    });
-};
-getUserData();
-}, [supabase.auth]);
+  //   useEffect(() => {
+  //     const getUserData = async () => {
+  //       await supabase.auth.getUser().then((value) => {
+  //         if (value.data?.user) {
+  //           if (value.data.user.identities[0].identity_data.name) {
+  //             //set discord name
+  //             setUser(value.data.user.identities[0].identity_data.name);
+  //           } else {
+  //             //set email name
+  //             setUser(value.data.user.identities[0].identity_data.email.split("@")[0]);
+  //         }
+  //         }
+  //     });
+  // };
+  // getUserData();
+  // }, [supabase.auth]);
+
+  // Redirect the user to the Discord authorization URL
+
+  const handleDiscordLogin = () => {
+    // Perform the necessary logic to redirect the user to the Discord authorization URL
+    // You can use window.location.href = AUTH_URL or use a library like react-router-dom to handle the redirection.
+    // Make sure to replace AUTH_URL with the actual Discord authorization URL.
+    window.location.href = discordAuthUrl;
+  };
 
   return (
     <div className="nav">
-      <Link to='/'>
-      <img
-        src={require("../../images/yt-thumbnail.png")}
-        alt="cautionfun logo"
-        className="caution-pic-nav"
-      />
+      <Link to="/">
+        <img
+          src={require("../../images/yt-thumbnail.png")}
+          alt="cautionfun logo"
+          className="caution-pic-nav"
+        />
       </Link>
       <div className="nav-container">
-      <div className={showMenu ? "login-modal" : "login-modal hidden"}>
-        <SupaBaseAuth showMenu={showMenu} setShowMenu={setShowMenu} setUser={setUser} user={user}/>
-      </div>
+        <div className={showMenu ? "login-modal" : "login-modal hidden"}>
+          {/* <SupaBaseAuth showMenu={showMenu} setShowMenu={setShowMenu} setUser={setUser} user={user}/> */}
+        </div>
         <Link to="/" className="nav-button">
           <div>Home</div>
         </Link>
@@ -68,12 +76,20 @@ getUserData();
           to="https://cautionfun.square.site"
           target="blank"
           className="nav-button"
-          >
+        >
           <div>Merch</div>
         </Link>
 
-          { !showMenu && <button onClick={openMenu} className="nav-button">{user ? user : "Sign In"}</button> }
-          { showMenu && <button onClick={closeMenu} className="nav-button">{user ? user : "Sign In"}</button> }
+        {!showMenu && (
+          <button onClick={handleDiscordLogin} className="nav-button">
+            {user ? user : "Sign In"}
+          </button>
+        )}
+        {showMenu && (
+          <button onClick={closeMenu} className="nav-button">
+            {user ? user : "Sign In"}
+          </button>
+        )}
       </div>
     </div>
   );
