43ffd2433e26d236423cc15f3fadfe450c7c2eea
RMPasta <rmpasta.code@gmail.com>
2023-07-12 18:26:23 -0400

    attemp heroku set up

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-#initialize flask app
+# Initialize Flask app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
